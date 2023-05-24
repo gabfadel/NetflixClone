12f197fd5af30ee03b5d0767a97ebce5d7a4f2a6
gabriel.faria-at-325485480401 <A/PpARTh6b6kxlSwx98GMe0TxlahiISbPK8z2URXZjY=>
2023-05-24 05:50:29 -0300

    routes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,27 @@
 import Image from 'next/image'
+import { getSession, signOut } from 'next-auth/react'
+import { NextPageContext } from 'next'
 
+export async function getServerSideProps(context:NextPageContext){
+  const session= await getSession(context);
+
+  if(!session){
+    return{
+      redirect:{
+        destination:'/auth',
+        permanent:false,
+      }
+    }
+  }
+  return{
+    props:{}
+  }
+}
 
 export default function Pages() {
   return (
-   <><h1 className='text-2xl text-green-500'>Netflix Clone</h1></>
+   <><h1 className='text-4xl text-green-500'>Netflix Clone</h1>
+   <button className='h-10 w-full bg-white' onClick={()=> signOut()}> sign Out!</button>
+   </>
   )
 }
